9442f48d2b0926635416d8d97cd8c87b21130857
kutaydede <113550112+kutaydede@users.noreply.github.com>
2023-12-22 16:23:56 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Gazi.OdevApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.grpOgrenci = new System.Windows.Forms.GroupBox();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblNumara = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.grpOgrenci.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(163, 240);
+            this.btnKaydet.Margin = new System.Windows.Forms.Padding(4);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(133, 28);
+            this.btnKaydet.TabIndex = 13;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // grpOgrenci
+            // 
+            this.grpOgrenci.Controls.Add(this.txtNumara);
+            this.grpOgrenci.Controls.Add(this.lblAd);
+            this.grpOgrenci.Controls.Add(this.lblSoyad);
+            this.grpOgrenci.Controls.Add(this.lblNumara);
+            this.grpOgrenci.Controls.Add(this.txtSoyad);
+            this.grpOgrenci.Controls.Add(this.txtAd);
+            this.grpOgrenci.Location = new System.Drawing.Point(52, 32);
+            this.grpOgrenci.Margin = new System.Windows.Forms.Padding(4);
+            this.grpOgrenci.Name = "grpOgrenci";
+            this.grpOgrenci.Padding = new System.Windows.Forms.Padding(4);
+            this.grpOgrenci.Size = new System.Drawing.Size(281, 187);
+            this.grpOgrenci.TabIndex = 12;
+            this.grpOgrenci.TabStop = false;
+            this.grpOgrenci.Text = "Öğrenci Bilgileri";
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Location = new System.Drawing.Point(111, 138);
+            this.txtNumara.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(132, 22);
+            this.txtNumara.TabIndex = 3;
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(39, 46);
+            this.lblAd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(24, 16);
+            this.lblAd.TabIndex = 4;
+            this.lblAd.Text = "Ad";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(39, 90);
+            this.lblSoyad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(47, 16);
+            this.lblSoyad.TabIndex = 5;
+            this.lblSoyad.Text = "Soyad";
+            // 
+            // lblNumara
+            // 
+            this.lblNumara.AutoSize = true;
+            this.lblNumara.Location = new System.Drawing.Point(39, 138);
+            this.lblNumara.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNumara.Name = "lblNumara";
+            this.lblNumara.Size = new System.Drawing.Size(55, 16);
+            this.lblNumara.TabIndex = 6;
+            this.lblNumara.Text = "Numara";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(111, 81);
+            this.txtSoyad.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(132, 22);
+            this.txtSoyad.TabIndex = 2;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(111, 37);
+            this.txtAd.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(132, 22);
+            this.txtAd.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 281);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.grpOgrenci);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpOgrenci.ResumeLayout(false);
+            this.grpOgrenci.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.GroupBox grpOgrenci;
+        private System.Windows.Forms.TextBox txtNumara;
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblNumara;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+    }
+}
+
---
+++ OkulApp.BLL/OgrenciBL.cs
@@ -0,0 +1,46 @@
+﻿using DAL;
+using OkulApp.MODEL;
+using System;
+using System.Data.SqlClient;
+
+namespace OkulApp.BLL//Bussiness Logic Layer
+{
+    public class OgrenciBL
+    {
+        //öğretmen tablosu için => tc kimlik ad soyad
+        public bool OgrenciEkle(Ogrenci ogr)
+        {
+            try
+            {
+                SqlParameter[] p = {
+                                  new SqlParameter("@Ad",ogr.Ad),
+                                  new SqlParameter("@Soyad",ogr.Soyad),
+                                  new SqlParameter("@Numara",ogr.Numara)
+                            };
+
+                var hlp = new Helper();
+                return hlp.ExecuteNonQuery("Insert into Ogrenci (Ad,Soyad,Numara) values (@Ad,@Soyad,@Numara)", p) > 0;
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                //if (cn != null && cn.State != ConnectionState.Closed)
+                //{
+                //    //cn.Close();
+                //    cn.Dispose();
+
+                //    cmd.Dispose();
+                //}
+            }
+        }
+
+
+    }
+}
